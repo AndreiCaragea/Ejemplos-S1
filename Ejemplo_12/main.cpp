e1daedb7120845fdcd964f4fb360932e2c6c8831
AndreiCaragea <andicaragea7@gmail.com>
2025-09-25 20:57:08 +0200

    Añadir ejemplos de la sesión 2

---
+++ Ejemplo_12/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+struct persona {
+    char nombre[30];
+    int edad;
+    long telefono;
+};
+
+void Escribir(persona p) // paso por valor
+{
+    cout << p.nombre << " tiene " << p.edad 
+         << " años y su telefono es " << p.telefono;
+    cout << endl;
+}
+
+void EscribirPuntero(persona* p) // paso por puntero
+{
+    cout << p->nombre << " tiene " << p->edad 
+         << " años y su telefono es " << p->telefono;
+    cout << endl;
+}
+
+persona CrearPersona(char n[30], int e, long t)
+{
+    persona aux;
+    strcpy(aux.nombre, n);   // copia la cadena n en aux.nombre
+    aux.edad = e;
+    aux.telefono = t;
+    return aux;
+}
+
+int main(int argc, char** argv)
+{
+    persona ejemplo;
+
+    ejemplo = CrearPersona((char*)"Jesus", 99, 123456789);
+    cout << "Paso por valor" << endl;
+    Escribir(ejemplo);
+    cout << endl;
+
+    ejemplo = CrearPersona((char*)"Mario", 55, 987654321);
+    cout << "Paso con punteros" << endl;
+    EscribirPuntero(&ejemplo);
+    return 0;
+}
